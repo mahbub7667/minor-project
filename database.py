0664b62de11094dfd83ee7431072757bdddf0132
mahbub7667 <mahbubgaming024@gmail.com>
2026-03-16 12:44:42 +0530

    Update database configuration to use dotenv

--- database.py
+++ database.py
@@ -1,8 +1,17 @@
-# database.py
+import os
+from dotenv import load_dotenv
 from supabase import create_client, Client
 
-SUPABASE_URL = "https://your-project-id.supabase.co"
-SUPABASE_KEY = "your-anon-public-key"
+# load the .env variables
+load_dotenv()
 
-# Client initialize karein
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+# take the keys form the environment
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+# initialize c;ient
+if not url or not key:
+    print("Galti: .env file mein keys nahi mili!")
+else:
+    supabase: Client = create_client(url, key)
+    print("Supabase connection set ho gaya!")
